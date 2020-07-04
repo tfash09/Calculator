93f8fae44d06543e7efa193d702d443778c8e714
Tunde Siyanbola <tsiyanbola@sidmach.com>
2020-07-04 11:11:27 +0100

    second commit

---
+++ src/component/Button.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Button extends Component{
+    render(){
+        return(
+            <div>
+                <button className="">{this.props.symbol}</button>
+            </div>
+        )
+    }
+}
+
+export default Button
